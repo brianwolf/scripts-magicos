26194001dee44c1fb171865a49c6efd9b7ef953e
Brian <brian.d.lobo@gmail.com>
2020-05-02 23:36:47 -0300

    actualizo scripts de docker y python

--- python/actualizar-requirements.sh
+++ python/actualizar-requirements.sh
@@ -1,6 +1,6 @@
 . ./scripts/python/ambiente.sh
 
 . $CARPETA_ENV/bin/activate &&
-    pip install -r $CARPETA_REQUIREMENTS &&\
+    pip install -r $CARPETA_REQUIREMENTS --upgrade pip &&\
     pip freeze > $CARPETA_REQUIREMENTS &&\
     deactivate
