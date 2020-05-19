86370316d54682e5d36d544f1480c9299246744d
Brian <brian.d.lobo@gmail.com>
2020-05-19 00:04:36 -0300

    nueva version de los scripts de python

--- python/actualizar-requirements.sh
+++
@@ -1,6 +0,0 @@
-. ./scripts/python/ambiente.sh
-
-. $CARPETA_ENV/bin/activate &&
-    pip install -r $CARPETA_REQUIREMENTS --upgrade pip &&\
-    pip freeze > $CARPETA_REQUIREMENTS &&\
-    deactivate
---
+++ python/iniciar.sh
@@ -0,0 +1,8 @@
+. ./scripts/python/ambiente.sh
+
+cd $CARPETA_CODIGO
+
+. env/bin/activate &&
+    python app.py
+
+cd ..
